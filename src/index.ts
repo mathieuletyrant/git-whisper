8ad4250978b61d6dc4c2d505bacf63688770e212
Mathieu Le Tyrant <mathieu.letyrant@gmail.com>
2024-11-11 21:30:07 +0100

    chore(version): bump version to 1.2.1

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ Config.initialize();
 const program = new Command();
 
 program
-  .version('1.2.0')
+  .version('1.2.1')
   .description(
     'Git Whisper is an intelligent CLI tool that generates meaningful and consistent commit messages using AI. Stop struggling with commit message writing and let AI help you create clear, concise, and conventional commits.',
   );
